50a1fd1c248fac619c1255226374c27ee8f8813a
titan <titan@Titan-PC.localdomain>
2019-09-26 13:57:26 +0800

    deleted:    Email.php 	modified:   test/EmailTest.php

--- test/EmailTest.php
+++ test/EmailTest.php
@@ -1,9 +1,11 @@
 <?php
 declare(strict_types=1);
 
+//require 'allAutoload.php';
 use PHPUnit\Framework\TestCase;
 
-final class EmailTest extends TestCase
+//final class EmailTest extends TestCase
+final class EmailTest extends PHPUnit_Framework_TestCase
 {
     public function testCanBeCreatedFromValidEmailAddress(): void
     {
